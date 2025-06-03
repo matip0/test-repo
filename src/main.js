2882190391bcad66d5bb78199637b2b994f9dd10
matip0 <78594634+matip0@users.noreply.github.com>
2025-06-03 21:36:17 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -58,3 +58,25 @@ function display(articles) {
 }
 
 articles();
+
+document.getElementById("addarticle").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const form = e.target;
+  const data = {
+    title: form.title.value,
+    subtitle: form.subtitle.value,
+    author: form.author.value,
+    content: form.content.value,
+  };
+
+  await fetch("https://dpdcjreodtodlmkpswzg.supabase.co/rest/v1/article", {
+    method: "POST",
+    headers: {
+      apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwZGNqcmVvZHRvZGxta3Bzd3pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NTM2NjcsImV4cCI6MjA2MzIyOTY2N30.QcfK3LEaNubuocLxBEHAdjFtswc-brvLXHfYtFYBsY4',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwZGNqcmVvZHRvZGxta3Bzd3pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NTM2NjcsImV4cCI6MjA2MzIyOTY2N30.QcfK3LEaNubuocLxBEHAdjFtswc-brvLXHfYtFYBsY4',
+    },
+    body: JSON.stringify(data)
+  });
+
+  articles();
+});
